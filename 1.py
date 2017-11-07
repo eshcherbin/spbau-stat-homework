036457d4478f8866f8b1971a519de9ae02c89a37
Egor Shcherbin <javamanse@ya.ru>
2017-11-07 17:03:04 +0300

    updated first hw

--- 1.py
+++ 1.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import math
 
 
-SAMPLE_SIZE = 100
-N = 1000
+SAMPLE_SIZE = 1000
+N = 10000
 
 def moments_estimate_uniform(sample, k):
     return (sum(map(lambda x: x**k, sample)) * (k + 1) / len(sample))**(1. / k)
@@ -15,8 +15,7 @@ def moments_estimate_exponential(sample, k):
     return (sum(map(lambda x: x**k, sample)) / (len(sample) * math.gamma(k + 1)))**(1. / k)
     
 
-def plot_estimate_variance(distribution, estimator):
-    ks = range(1, 11);
+def plot_estimate_variance(distribution, estimator, ks):
     es = [np.mean([(estimator(distribution(size=SAMPLE_SIZE), k) - 1)**2 for _ in range(N)]) for k in ks]
     plt.plot(ks, es)
     plt.xlabel('k')
@@ -28,13 +27,13 @@ if __name__ == '__main__':
     plt.subplot(121)
 
     # uniform distribution
-    plot_estimate_variance(np.random.uniform, moments_estimate_uniform)
+    plot_estimate_variance(np.random.uniform, moments_estimate_uniform, np.linspace(1, 11, 20))
     plt.title('Uniform distribution')
 
     plt.subplot(122)
 
     # exponential distribution
-    plot_estimate_variance(np.random.exponential, moments_estimate_exponential)
+    plot_estimate_variance(np.random.exponential, moments_estimate_exponential, np.linspace(0.2, 2.0, 20))
     plt.title('Exponential distribution')
 
 
